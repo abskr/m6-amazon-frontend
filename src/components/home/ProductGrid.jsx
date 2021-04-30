e946b3e821f0f1ca008b6d9f7af40eca5bbbe277
Sean Knowles <s@seanknowles.dev>
2021-04-30 14:21:52 +0100

    homepage product grid api fetch and display products working

--- src/components/home/ProductGrid.jsx
+++ src/components/home/ProductGrid.jsx
@@ -2,26 +2,24 @@ import React, {Component} from 'react';
 import axios from 'axios'
 import styled from 'styled-components'
 
-import productDoc from 'data/products.doc.json'
 import ProductCard from "../ui/ProductCard.jsx";
 
 class ProductGrid extends Component {
   state = {
-    products: productDoc.products,
     apiProducts: []
   }
 
   async componentDidMount() {
     console.log(this.state.products)
-    const {data} = axios('https://m6-amazon-backend.herokuapp.com/products/')
-    console.log(data)
+    const {data} = await axios('https://m6-amazon-backend.herokuapp.com/products/')
+    this.setState({...this.state, apiProducts: data.products})
   }
 
   render() {
     return (
       <GridWrapper>
-        {this.state.products.map((product, i) => {
-          return <ProductCard {...product} key={i} />
+        {this.state.apiProducts.map((product, i) => {
+          return <ProductCard {...product} key={i}/>
         })}
       </GridWrapper>
     );
