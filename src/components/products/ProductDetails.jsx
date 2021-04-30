8377838f1db16f7bd6def0738fd60579d4091072
Sean Knowles <s@seanknowles.dev>
2021-04-30 16:25:26 +0100

    product details page update

--- src/components/products/ProductDetails.jsx
+++ src/components/products/ProductDetails.jsx
@@ -1,48 +1,48 @@
-import React from 'react';
-import { Col , Container, Row} from 'react-bootstrap'
+import React, {Component} from 'react';
+import {Col, Container, Row} from 'react-bootstrap'
 import styled from 'styled-components'
 import StarRatingComponent from 'react-star-rating-component';
 import PropTypes from "prop-types";
 
-const ProductPage = () => {
+class ProductPage extends Component {
+  state = {
+    isLoading: true,
+    product: {}
+  }
 
-    state = {
-      isLoading: true,
-      product : {}
-    }
-    fetchProduct () {
-  
-    const { handle } = this.props.match.params
-    fetch(`https://m6-amazon-backend.herokuapp.com/product/:${handle}`)
-    .then(response => response.json())
-    .then(data => this.setState({ product: data, isLoading: false}))
-    .catch(error => this.setState({ error, isLoading: false }));
-    }
+  async componentDidMount() {
+    const {productId} = this.props.matched.params
+    const {data} = await axios.get(`https://m6-amazon-backend.herokuapp.com/products/${productId}`)
+    this.setState({...this.state, product: data})
+  }
 
-    const { isLoading, product, error } = this.state;
+  render() {
     return (
-    
+
       <div>
         <ProductContainer>
           <Row>
             <Col className='col-3'>
-              <img src="https://cdn.phonehouse.es/res/viewthree450/resource_1021924.jpg?&w=380&h=380&trim=auto&auto=format" alt=""/>
+              <img
+                src="https://cdn.phonehouse.es/res/viewthree450/resource_1021924.jpg?&w=380&h=380&trim=auto&auto=format"
+                alt=""/>
               <h5>{product.image}</h5>
             </Col>
             <Col className='col-6'>
-            <h5>{product.name}</h5>
-            <h5>{product.brand}</h5>
-            <h5>{product.category}</h5>
-            <p>{product.description}</p>
+              <h5>{product.name}</h5>
+              <h5>{product.brand}</h5>
+              <h5>{product.category}</h5>
+              <p>{product.description}</p>
             </Col>
           </Row>
         </ProductContainer>
       </div>
     );
-  };
-  
-  const ProductContainer = styled(Container)`
-    margins: 0;
-  `
-  export default ProductPage;
-  
+  }
+  ;
+}
+
+const ProductContainer = styled(Container)`
+  margins: 0;
+`
+export default ProductPage;
