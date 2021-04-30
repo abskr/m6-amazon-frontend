53d3481a715c2422820fdf884081e2e68fa7e2b8
Jamie Ellis <78352327+Jamie-Ellis@users.noreply.github.com>
2021-04-30 17:04:29 +0200

    up

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,34 +1,34 @@
-import React, {Component} from 'react';
-import {Col, Container, Row} from 'react-bootstrap'
-import styled from 'styled-components'
-import axios from 'axios'
-import StarRatingComponent from 'react-star-rating-component';
+import React, { Component } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import styled from "styled-components";
+import axios from "axios";
+import StarRatingComponent from "react-star-rating-component";
 import PropTypes from "prop-types";
 
 class ProductPage extends Component {
   state = {
-    product: {}
-  }
+    product: {},
+  };
 
   componentDidMount = async () => {
-    const {productId} = this.props.match.params
-    console.log(this.props.match.params)
-    const {data} = await axios.get(`https://m6-amazon-backend.herokuapp.com/products/${productId}`)
-    console.log(data)
+    const { productId } = this.props.match.params;
 
-  }
+    const { data } = await axios.get(
+      `https://m6-amazon-backend.herokuapp.com/products/${productId}`
+    );
+    this.setState({ ...this.state, product: data });
+  };
 
   render() {
+    console.log(this.state);
     return (
       <div>
         <ProductContainer>
           <Row>
-            <Col className='col-3'>
-              <img
-                src={this.state.product.image}
-                alt=""/>
+            <Col className="col-3">
+              <img src={this.state.product.image} alt="" />
             </Col>
-            <Col className='col-6'>
+            <Col className="col-6">
               <h5>{this.state.product.name}</h5>
               <h5>{this.state.product.brand}</h5>
               <p>{this.state.product.description}</p>
@@ -37,10 +37,10 @@ class ProductPage extends Component {
         </ProductContainer>
       </div>
     );
-  };
+  }
 }
 
 const ProductContainer = styled(Container)`
   margins: 0;
-`
+`;
 export default ProductPage;
